cbd166b2195358e1626c3a563e8ae47fbbfd0607
Matias Pariente <matiaspariente@hotmail.com>
2022-07-03 09:59:35 -0300

    Prueba sessions Heroku

--- src/config.js
+++ src/config.js
@@ -4,7 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 (async () => {
-    const CS = process.env._MONGO_URL
+    //const CS = process.env._MONGO_URL
+    const CS = "mongodb+srv://coderhouse:coderhouse@matiaspariente.qctoeul.mongodb.net/?retryWrites=true&w=majority"
     try {
         await mongoose.connect(CS);
         console.log('MongoDB connected');
